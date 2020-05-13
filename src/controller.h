a94ef7cdbe836de0f7bb795daad6c87f2fbb8a67
Mohamed Hendawy <mohamedhendawy312@gmail.com>
2020-05-13 19:13:34 +0200

    Update controller.h

--- src/controller.h
+++ src/controller.h
@@ -1,12 +1,3 @@
-/*Skip to content
- Nickdom1 / CppND-Capstone-Snake-Game
-Code Pull requests 0 Projects 0 Actions Wiki Security 0 Pulse
-CppND-Capstone-Snake-Game/src/controller.h
-@Nickdom1 Nickdom1 Add files via upload
-cec5791 on Oct 10, 2019
-@swwelch@Nickdom1
-20 lines (15 sloc)  546 Bytes
-*/ 
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
@@ -27,16 +18,3 @@ class Controller {
 };
 
 #endif
-/*© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-*/
